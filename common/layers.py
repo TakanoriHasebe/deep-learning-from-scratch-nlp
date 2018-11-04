b37f365690e401839dd6ea6670d2c0ab7ae42f2a
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2018-11-05 08:05:02 +0900

    続きから

---
+++ common/layers.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        # print(self.params)
+        self.grads = [np.zeros_like(W)]
+        # print(self.grads)
+        self.x = None
+
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        print(self.grads)
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
